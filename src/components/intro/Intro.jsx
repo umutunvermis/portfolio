c4eaf7a1b5fb1956392ff496604f190578bec712
Umut ÜNVERMİŞ <105220352+umutunvermis@users.noreply.github.com>
2024-05-18 22:10:52 +0300

    Update Intro.jsx
    i have graduated :P long time ago

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -9,15 +9,15 @@ export default function Intro() {
       <div className="right">
         <h2>Hello I'm</h2>
         <h1>Umut ÜNVERMİŞ</h1>
-        <h3>Web Developer</h3>
-        <h3>3rd Grade Computer Engineering Stutent at IZTECH</h3>
+        <h3>Full Stack Developer</h3>
+        <h3>Computer Engineer</h3>
 
         <div className="about">
             <p>
               I like working on web projects mostly on back-end side,
               but I am also enthusiastic about designing. I am experienced
-              on php HTML and SQL databases. I have practice on JavaScript- 
-              JQuery-React. I also have some low level server projects on C.
+              on .Net with C#. I have practice on JavaScript- 
+              JQuery-React.
 
             </p>
         </div>
